8a340a25299901db213a20bc7c71b5d14fdbc54b
hyunjoon <shj2322@gmail.com>
2024-08-05 17:17:02 +0900

    v4

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello React And Docker V3
+          Hello React And Docker V4
         </p>
         <a
           className="App-link"
